aba9055c275da954f2f998978596c3303dca3c9e
hossam ameen <Hosam.15237512@compit.au.edu.eg>
2019-01-28 15:03:27 +0200

    C2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         factory('App\User',1)->create();
         factory('App\Pref',1)->create();
         factory('App\Day',1)->create();
-
+        factory('App\Service',6)->create();
        /* factory('App\Service',6)->create();
           factory('App\Product',10)->create();*/
        
