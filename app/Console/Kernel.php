adeac868162d09c1613ff0e759c42b71c2311bf4
Abdelrahman <abdelrahman.elzedy@gmail.com>
2020-02-04 09:16:13 +0200

    general updates 5

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,8 +33,8 @@ class Kernel extends ConsoleKernel
 //            }
 //        })->everyMinute();
 
-//        $schedule->command('copy:day')->dailyAt('00:01');
-         $schedule->command('copy:day')->everyMinute();
+        $schedule->command('copy:day')->dailyAt('00:01');
+//         $schedule->command('copy:day')->everyMinute();
     }
 
     /**
